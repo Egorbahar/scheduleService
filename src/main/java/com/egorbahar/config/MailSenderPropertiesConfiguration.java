206956d0154b182e3f126539638ba8115b3fd415
Egorbahar <egorbahar@gmail.com>
2022-01-18 00:43:02 +0300

    Add email configuration

---
+++ src/main/java/com/egorbahar/config/MailSenderPropertiesConfiguration.java
@@ -0,0 +1,30 @@
+package com.egorbahar.config;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+public class MailSenderPropertiesConfiguration {
+    @Bean
+    public JavaMailSender getJavaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+
+        mailSender.setUsername("egorbahar@gmail.com");
+        mailSender.setPassword("12345005e");
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.debug", "true");
+
+        return mailSender;
+    }
+}
